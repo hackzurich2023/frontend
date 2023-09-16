decb4570b7a2b4843594ab744737593cca6dab47
Diego Nuñez <diego.gabriel.nd@gmail.com>
2023-09-16 11:43:37 +0200

    Move account page to component

---
+++ src/components/AccountPage.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+
+export function AccountPage() {
+    return (
+      <section className="pt-4">
+      <div className="container px-lg-5">
+        <div className="row gx-lg-5">
+          <div className="col-lg-6 col-xxl-4 mb-5">
+            <div className="card bg-light border-0 h-100">
+              <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                <div
+                  className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"
+                >
+                  <i className="bi bi-collection"></i>
+                </div>
+                <h2 className="fs-4 fw-bold">Buy a SkiPass</h2>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-6 col-xxl-4 mb-5">
+            <div className="card bg-light border-0 h-100">
+              <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                <div
+                  className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"
+                >
+                  <i className="bi bi-cloud-sun-fill"></i>
+                </div>
+                <h2 className="fs-4 fw-bold">Live Weather Update</h2>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-6 col-xxl-4 mb-5">
+            <div className="card bg-light border-0 h-100">
+              <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                <div
+                  className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"
+                >
+                  <i className="bi bi-map"></i>
+                </div>
+                <h2 className="fs-4 fw-bold">Interactive Ski Map</h2>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-6 col-xxl-4 mb-5">
+            <div className="card bg-light border-0 h-100">
+              <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                <div
+                  className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"
+                >
+                  <i className="bi bi-stars"></i>
+                </div>
+                <h2 className="fs-4 fw-bold">Events</h2>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-6 col-xxl-4 mb-5">
+            <div className="card bg-light border-0 h-100">
+              <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                <div
+                  className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"
+                >
+                  <i className="bi bi-house"></i>
+                </div>
+                <h2 className="fs-4 fw-bold">Rental</h2>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-6 col-xxl-4 mb-5">
+            <div className="card bg-light border-0 h-100">
+              <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                <div
+                  className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"
+                >
+                  <i className="bi bi-cart-plus-fill"></i>
+                </div>
+                <h2 className="fs-4 fw-bold">Food and drinks</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -3,10 +3,15 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 
 import { Home } from "components/Home";
+import {AccountPage} from 'components/AccountPage';
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
   },
+  {
+    path: '/account',
+    element: <AccountPage />
+  }
 ]);
