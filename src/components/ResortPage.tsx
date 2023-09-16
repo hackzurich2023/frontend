c105945ecba38d9dffd001f5c428d70f37f1c14d
Diego Nuñez <diego.gabriel.nd@gmail.com>
2023-09-16 23:11:14 +0200

    Add section contents dynamically

--- src/components/ResortPage.tsx
+++ src/components/ResortPage.tsx
@@ -1,5 +1,5 @@
 import { MainLayout } from "MainLayout";
-import React from "react";
+import React, { ReactNode, useMemo } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCalendar,
@@ -15,54 +15,69 @@ import { ResortName } from "data/resorts";
 
 import { NavigationTab } from "components/NavigationTabButton";
 import { DEFAULT_USER } from "data/user";
+import { Container } from "react-bootstrap";
+import { SnowReport } from "components/SnowReport";
 
-const TABS: Omit<NavigationTab, "routerTo">[] = [
+const TABS: (Omit<NavigationTab, "routerTo"> & { content: ReactNode })[] = [
   {
     id: "snow_reports",
     name: "Snow Report",
     icon: <FontAwesomeIcon icon={faSnowflake} />,
+    content: <SnowReport />,
   },
   {
     id: "ski_pass",
     name: "Buy a Ski Pass",
     icon: <FontAwesomeIcon icon={faTicket} />,
+    content: <h1>Buy a Ski Pass</h1>,
   },
   {
     id: "ski_map",
     name: "Ski Map",
     icon: <FontAwesomeIcon icon={faMap} />,
+    content: <h1>Ski Map</h1>,
   },
   {
     id: "rental",
     name: "Rental",
     icon: <FontAwesomeIcon icon={faPersonHiking} />,
+    content: <h1>Rental</h1>,
   },
   {
     id: "events",
     name: "Events",
     icon: <FontAwesomeIcon icon={faCalendar} />,
+    content: <h1>Events</h1>,
   },
   {
     id: "food_and_drinks",
     name: "Food & Drinks",
     icon: <FontAwesomeIcon icon={faUtensils} />,
+    content: <h1>Food & Drinks</h1>,
   },
 ];
 
 function useResortTabs(resortName: ResortName) {
-  return TABS.map((tab) => ({
+  return TABS.map(({ content, ...tab }) => ({
     ...tab,
     routerTo: `/resorts/${resortName}/${tab.id.toLowerCase()}`,
   }));
 }
 
 export function ResortPage() {
-  const { resortName } = useParams();
+  const { resortName, section } = useParams();
   const resortTabs = useResortTabs(resortName as ResortName);
+  const currentContent = useMemo(
+    () =>
+      TABS.filter(
+        (tab, index) => (!section && index === 0) || tab.id === section
+      )[0]?.content,
+    [section]
+  );
 
   return (
     <MainLayout tabs={resortTabs} currentUser={DEFAULT_USER}>
-      <h1>Resort Page</h1>
+      <Container className="p-4">{currentContent}</Container>
     </MainLayout>
   );
 }
---
+++ src/components/SnowReport.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function SnowReport() {
+  return <h1>Snow Report</h1>;
+}
